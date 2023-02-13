f2bd42a453e48541d3956d0068e0f2ab03348e1f
fmarzulli <f.marzulli@ads.it>
2023-02-13 13:27:54 +0100

    Aggiunti script creazione function

---
+++ db/latest/fun/get_id_regione.sql
@@ -0,0 +1,28 @@
+--liquibase formatted sql
+
+--changeset mturra:201901301231_96 runOnChange:true endDelimiter:/ stripComments:false
+
+
+CREATE OR REPLACE FUNCTION Get_Id_Regione
+/******************************************************************************
+ NOME:        GET_ID_REGIONE
+ DESCRIZIONE: Dato un codice regione, ritorna il suo id.
+ PARAMETRI:   p_regione number Codice regione.
+ RITORNA:     number id_regione
+ ECCEZIONI:   -
+ ANNOTAZIONI: Salvata nella directory ins di AD4 con nome GET_IDREG.CRF.
+ REVISIONI:
+ Rev. Data       Autore Descrizione
+ ---- ---------- ------ ------------------------------------------------------
+ 0    03/02/2004 MM     Prima emissione.
+ 1    01/12/2006 MM     Richiamo funzione di package.
+******************************************************************************/
+( p_regione NUMBER)
+RETURN NUMBER
+IS
+BEGIN
+   RETURN Regione.get_id(p_regione);
+END;
+
+/
+
---
+++ db/latest/fun/traccia_accesso.sql
@@ -0,0 +1,41 @@
+--liquibase formatted sql
+
+--changeset mturra:201901301231_98 runOnChange:true endDelimiter:/ stripComments:false
+
+
+CREATE OR REPLACE FUNCTION Traccia_Accesso
+( p_session_id NUMBER
+, p_istanza VARCHAR2
+, p_modulo VARCHAR2
+, p_utente VARCHAR2
+, p_testo VARCHAR2
+)
+RETURN NUMBER IS
+   PRAGMA AUTONOMOUS_TRANSACTION;
+tmpVar NUMBER;
+/******************************************************************************
+ NOME:        TRACCIA_ACCESSO
+ DESCRIZIONE: .
+ PARAMETRI:   p_session_id number     numero identificativo della sessione
+              p_istanza    varchar2   codice istanza a cui l'utente e connesso
+              p_modulo     varchar2   codice modulo a cui l'utente e connesso
+              p_utente     varchar2   codice utente applicativo connesso
+           p_testo      varchar2   testo della traccia da registrare
+ RITORNA:     number: identificativo dell'accesso registrato.
+ ANNOTAZIONI: Salvata nella directory ins di AD4 con nome TRAC.CRF.
+ REVISIONI:
+ Rev. Data       Autore Descrizione
+ ---- ---------- ------ ------------------------------------------------------
+ 0    24/05/2005 MM     Prima emissione.
+******************************************************************************/
+BEGIN
+  tmpVar := Accesso.REGISTRA_ACCESSO(p_session_id, 'TRC', p_istanza, p_modulo, p_utente, p_testo, NULL);
+  COMMIT;
+  RETURN tmpVar;
+EXCEPTION
+   WHEN OTHERS THEN
+      ROLLBACK;
+      RETURN -1;
+END Traccia_Accesso;
+/
+
